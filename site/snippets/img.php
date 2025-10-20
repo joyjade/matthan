6fe579b10d06c0edd21d1bb3e0d9f23f25165c51
JJ <jadeandpearls@gmail.com>
2025-10-20 09:26:04 -0600

    attempt to fix srcset imaging;

--- site/snippets/img.php
+++ site/snippets/img.php
@@ -1,12 +1,14 @@
 
+<!-- for works or exhibitions pages where the pages link to another page and not a lightbox image -->
+
 <figure>
   <img
       loading="lazy"
       alt="<?= $photo->alt() ?>"
-      src="<?= $photo->resize(400)->url() ?>"
-      srcset="<?= $photo->srcset([400, 600, 900, 1200, 1800]) ?>"
-      width="<?= $photo->resize(1800)->width() ?>"
-      height="<?= $photo->resize(1800)->height() ?>"
+      src="<?= $photo->url() ?>"
+      srcset="<?= $photo->srcset([400, 600]) ?>"
+      width="<?= $photo->width() ?>"
+      height="<?= $photo->height() ?>"
       aria-describedby="materials"
   >
 </figure>
--- site/snippets/lightbox-img.php
+++ site/snippets/lightbox-img.php
@@ -3,10 +3,10 @@
   <img
       loading="lazy"
       alt="<?= $photo->alt() ?>"
-      src="<?= $photo->resize(400)->url() ?>"
-      srcset="<?= $photo->srcset([400, 600, 900, 1200, 1800]) ?>"
-      width="<?= $photo->resize(1800)->width() ?>"
-      height="<?= $photo->resize(1800)->height() ?>"
+      src="<?= $photo->url() ?>"
+      srcset="<?= $photo->srcset([400, 900, 1800]) ?>"
+      width="<?= $photo->width() ?>"
+      height="<?= $photo->height() ?>"
       aria-describedby="materials"
   >
   <?php if($photo->caption()->isNotEmpty()) : ?>
