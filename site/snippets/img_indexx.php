0be7214e30b81c5a7714ea69a51f48d25c422a32
JJ <jadeandpearls@gmail.com>
2025-10-20 14:10:32 -0600

    height fix for phones;

---
+++ site/snippets/img_indexx.php
@@ -0,0 +1,14 @@
+
+<!-- for works or exhibitions pages where the pages link to another page and not a lightbox image -->
+
+<figure>
+  <img
+      loading="<?= $index < 4 ? 'eager' : 'lazy' ?>"
+      alt="<?= $photo->alt() ?>"
+      src="<?= $photo->url() ?>"
+      srcset="<?= $photo->srcset([400, 600]) ?>"
+      width="<?= $photo->width() ?>"
+      height="<?= $photo->height() ?>"
+      aria-describedby="materials"
+  >
+</figure>
